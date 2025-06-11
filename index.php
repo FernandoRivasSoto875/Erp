8d5a52dfb2c7a0e6598028ed0c4da7df90fb283f
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-06-11 00:14:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 // revisa el codigo anterior con copilot
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +14,7 @@
     <?php include 'redes.php'; ?>
    
     <main>
-        <h2>Bienvenido a  .....nuestro Servicio de Salud y Óptica empresarial en Terreno.</h2>
+        <h2>Bienvenido a  ..........nuestro Servicio de Salud y Óptica empresarial en Terreno.</h2>
         <h3>Nos acercamos a tu empresa para cuidar la salud visual y bienestar de tus colaboradores, brindando atención profesional y soluciones a su medida.</h3>
         
 

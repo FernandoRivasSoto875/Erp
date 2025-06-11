e5fed18b671580e375d795724c75beacb83d7266
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-06-10 23:51:28 -0400

    Update ApiTable.php

--- ApiTable.php
+++ ApiTable.php
@@ -4,7 +4,6 @@ error_reporting(E_ALL);
 ini_set('log_errors', 1);
 ini_set('display_errors', 0);
 ini_set('error_log', __DIR__ . "/error_log.txt"); // Archivo donde se guardarán los errores
-
 header('Content-Type: application/json');
 
 // Incluir el archivo con las funciones de conexión

e83658f348ecfa2b9868a86f510066deb97490ca
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-06-11 12:05:45 -0400

    Update Index.php

--- Index.php
+++ Index.php
@@ -14,7 +14,7 @@
     <?php include 'menu.php'; ?>
     <?php include 'redes.php'; ?>
        <main>
-        <h2>Bienvenido a .........nuestro Servicio de Salud y Óptica empresarial en Terreno.</h2>
+        <h2>Bienvenido a nuestro Servicio de Salud y Óptica empresarial en Terreno.</h2>
         <h3>Nos acercamos a tu empresa para cuidar la salud visual y bienestar de tus colaboradores, brindando atención profesional y soluciones a su medida.</h3>
  status        <img src="imagenes/inicioservicios.png" alt="Servicios" style="max-width: 400px; border-radius: 10px;">
         <p></p>

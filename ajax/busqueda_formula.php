94587889b24cf03341021df1c2890988ee580bc3
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-06-12 20:28:03 -0400

    Update busqueda_formula.php

--- ajax/busqueda_formula.php
+++ ajax/busqueda_formula.php
@@ -6,6 +6,9 @@ $conn = conexionBd();
 
 $data = json_decode(file_get_contents('php://input'), true);
 
+// DEBUG: Guarda los datos recibidos
+file_put_contents(__DIR__ . '/debug_busqueda.txt', print_r($data, true));
+
 // Validar nombres de tabla y campo (solo letras, números y guion bajo)
 $tabla = preg_replace('/\W/', '', $data['tabla']);
 $campo = preg_replace('/\W/', '', $data['campo']);
@@ -18,6 +21,9 @@ foreach ($where as $k => $v) {
     $valores[] = $v;
 }
 $whereSql = implode(' AND ', $condiciones);
+
+// DEBUG: Guarda la consulta SQL y los valores
+file_put_contents(__DIR__ . '/debug_sql.txt', "SELECT $campo FROM $tabla WHERE $whereSql LIMIT 1\n" . print_r($valores, true));
 
 $stmt = $conn->prepare("SELECT $campo FROM $tabla WHERE $whereSql LIMIT 1");
 if ($valores) {

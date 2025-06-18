a1b64f547b31a9897530fe3a9067f892934b297a
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-06-18 19:05:23 -0400

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,7 +1,6 @@
  
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,6 +21,3 @@
     <?php include 'footer.php'; ?>
 </body>
 </html>
-
-
- 

a417ce07686debda580f8020e7ca70a8cdb34de2
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-07-07 20:13:25 -0400

    Update formulariodinamico.funciones.php

--- formulariodinamico.funciones.php
+++ formulariodinamico.funciones.php
@@ -5,6 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 /**
+ * Refisar
  * Renderiza los fieldsets y sus campos en modo de solo lectura.
  * Ideal para correos, PDFs o vistas de resumen.
  */

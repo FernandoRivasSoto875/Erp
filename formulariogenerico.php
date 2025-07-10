f9e4cc08dceac113adfa61f7dc228ef46711bd5f
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-07-10 17:58:08 -0400

    Update formulariogenerico.php

--- formulariogenerico.php
+++ formulariogenerico.php
@@ -1,9 +1,10 @@
- 
+<!DOCTYPE html>
+<html lang="es">
+<body>
     <?php include 'header.php'; ?>
     <?php include 'menu.php'; ?>
     <?php include 'redes.php'; ?>
-
-    
+    <main>
           <?php
         // Ruta absoluta del archivo JSON
         $_GET['archivo'] = __DIR__ . '/json/formulariogenerico.json';
@@ -17,6 +18,8 @@
             echo "<p>Error: No se pudo cargar el formulario dinámico. Verifica la configuración.</p>";
         }
         ?>
-  
+    </main>
     <?php include 'footer.php'; ?>
- 
+</body>
+</html>
+

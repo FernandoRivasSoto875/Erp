5e4d6c9b4323ca8abd8b809c3afe494b6ea95dd3
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-07-12 14:28:44 -0400

    Update formulariodinamico.php

--- formulariodinamico.php
+++ formulariodinamico.php
@@ -71,4 +71,11 @@ window.validacionesJSON = <?php echo json_encode($json['parametros']['validacion
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="js/formulariodinamico.js"></script>
+<script>
+// Solo carga el JS si no está ya presente (para evitar duplicidad en modo embebido)
+if (!window.inicializarFormularioDinamico) {
+    var script = document.createElement('script');
+    script.src = 'js/formulariodinamico.js';
+    document.head.appendChild(script);
+}
+</script>

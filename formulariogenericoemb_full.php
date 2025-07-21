1a0e20339f3c6f4d4f01e58468dafff93d1ee54c
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-07-21 18:44:46 -0400

    commit

--- formulariogenericoemb_full.php
+++ formulariogenericoemb_full.php
@@ -19,7 +19,11 @@
     <?php include 'menu.php'; ?>
     <?php include 'redes.php'; ?>
     <main id="formulario-dinamico-main">
-        <iframe src="https://saludenterreno.cl/ErpQa/formulariodinamico.php?archivo=json/formulariogenerico2.json" width="100%" height="900" frameborder="0" style="border:none;min-height:600px;"></iframe>
+        <iframe src="https://saludenterreno.cl/ErpQa/formulariodinamico.php?archivo=json/formulariogenerico2.json"
+            style="width:100%;min-height:400px;max-height:90vh;border:none;overflow:auto;"
+            allowfullscreen
+            loading="lazy"
+            title="Formulario Dinámico Embebido"></iframe>
     </main>
     <?php include 'footer.php'; ?>
 </body>
--- formulariogenericoemb_innerhtml.php
+++ formulariogenericoemb_innerhtml.php
@@ -9,7 +9,11 @@
 </head>
 <body>
     <div style="max-width:900px;margin:2em auto;">
-        <iframe src="formulariodinamico.php?archivo=json/formulariogenerico2.json" width="100%" height="900" frameborder="0" style="border:none;min-height:600px;"></iframe>
+        <iframe src="formulariodinamico.php?archivo=json/formulariogenerico2.json" 
+            style="width:100%;min-height:400px;max-height:90vh;border:none;overflow:auto;"
+            allowfullscreen
+            loading="lazy"
+            title="Formulario Dinámico Embebido"></iframe>
     </div>
 </body>
 </html>

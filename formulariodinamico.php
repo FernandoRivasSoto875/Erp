373fdbcc1954580359a99fa6f5fd79c98832e9fc
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-08-09 16:28:34 -0400

    Update formulariodinamico.php

--- formulariodinamico.php
+++ formulariodinamico.php
@@ -190,5 +190,6 @@ require_once __DIR__ . '/formulariodinamicologica.php';
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="js/json-tree-panel.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>

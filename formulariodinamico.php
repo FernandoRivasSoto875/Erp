e3daf81048cd0d5d90ae81b3e500a356d00d0da1
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-08-20 09:31:03 -0400

    Update formulariodinamico.php

--- formulariodinamico.php
+++ formulariodinamico.php
@@ -114,18 +114,15 @@ $modoDiseno = isset($_GET['modoDiseno']) ? (bool)$_GET['modoDiseno'] : false;
       echo '</div>'; // fd-shell
       echo '</div>'; // container-fluid
 
-  <?php
     // Contenedor del árbol y nodo de datos
     echo '<div id="fd-json-tree-app" class="'.($modoDiseno ? '' : 'd-none').'" data-tree-app></div>';
     echo '<div id="fd-data"'
       .' data-json-file="'.htmlspecialchars($archivo_base ?? 'formulariogenerico2.json').'"'
       .' data-form-json=\''.htmlspecialchars(json_encode($json_data ?? [], JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES), ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8").'\'></div>';
-  ?>
-
+?>
   <!-- Scripts requeridos -->
   <!-- Scripts requeridos para render y diseño -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.2/Sortable.min.js"></script>
   <script src="js/formulariodinamico_v2.js"></script>
   <script src="js/formulariodinamico-init.js"></script>
-?>

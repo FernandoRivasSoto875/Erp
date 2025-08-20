52db57281431f5ea1849e6af1ab71b3299cde903
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-08-20 15:14:00 -0400

    Update formulariodinamico.php

--- formulariodinamico.php
+++ formulariodinamico.php
@@ -33,6 +33,9 @@ foreach ($fieldsets as $fs) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/css/estilos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <?php if (!empty($json['CssDefault'])): ?>
+    <link rel="stylesheet" href="/css/<?php echo htmlspecialchars($json['CssDefault']); ?>.css">
+    <?php endif; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="/js/formulariodinamico.js"></script>
     <script src="/js/formulariodinamico-page.js"></script>

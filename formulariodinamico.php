7a9d2e7a2881d605a3cab0331e5d9fa902fb0ab6
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-08-20 15:15:21 -0400

    Update formulariodinamico.php

--- formulariodinamico.php
+++ formulariodinamico.php
@@ -31,7 +31,6 @@ foreach ($fieldsets as $fs) {
     <meta charset="UTF-8">
     <title>Formulario Dinámico</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/estilos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <?php if (!empty($json['CssDefault'])): ?>
     <link rel="stylesheet" href="/css/<?php echo htmlspecialchars($json['CssDefault']); ?>.css">

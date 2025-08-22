830d5e87d4328f2e0f7837118be9ce721c0cd936
FernandoRivasSoto875 <Gestion@SaludEnTerreno.cl>
2025-08-21 21:54:48 -0400

    Update formulariodinamicologica.php

--- formulariodinamicologica.php
+++ formulariodinamicologica.php
@@ -4,6 +4,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require_once __DIR__ . '/fpdf/fpdf.php';
+require_once __DIR__ . '/formulariodinamicofunciones.php';
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
